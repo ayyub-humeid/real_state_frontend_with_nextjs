8c038b8a29bd41f55ecfeda634e64b0b6158db85
ayyub humeid <ayoubhumeid@gmail.com>
2026-08-07 17:10:32 +0300

    fix displaying images in next js

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'pub-6dd4e09e76534df396c797a48801a83a.r2.dev',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
